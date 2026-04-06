4b8540bcd1df71ad5088bd1b24e54d8c61a4764a
Douglas L <douglas.laraf@gmail.com>
2026-04-05 21:37:36 -0300

    Adiciona funcionalidades iniciais do blog

--- app/models.py
+++ app/models.py
@@ -101,6 +101,10 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
                                                 unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
                                              unique=True)
+
+
+    blog_post: so.Mapped[str] = so.mapped_column(sa.String(600), index=True, unique = True)
+
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
     about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
     last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
